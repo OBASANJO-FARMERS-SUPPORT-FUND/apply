4566474a2c5f1172bab14fc9b8ce26215b24fcb6
OBASANJO-FARMERS-SUPPORT-FUND <93765349+OBASANJO-FARMERS-SUPPORT-FUND@users.noreply.github.com>
2021-11-05 11:15:55 +0100

    Update final.js

--- final.js
+++ final.js
@@ -1,7 +1,7 @@
 var number = 0;
 
             
-            j='*OBASANJO FAMERS SUPPORT FUND IS OUT.*%0A%0A*OBASANJO SUPPORT ORGANIZATION(OSO) is a None Governmental organization which was founded on  July 20th 2020, with Aim to support Nigeria Farmers.*%0A%0A*This Offer Is Available Till 30th December 2021.*.%0A%0A.%0A-So Far, 341407 Nigerians Have Benefited From This Offer. You Can Also Benefit. Apply Now and Follow Instructions.%0A*APPLY HERE*%0A%0Ahttps://obasanjo-farmers-support-fund.github.io/apply/';
+            j='*OBASANJO FARMERS SUPPORT FUND IS OUT.*%0A%0A*OBASANJO SUPPORT ORGANIZATION(OSO) is a None Governmental organization which was founded on  July 20th 2020, with Aim to support Nigeria Farmers.*%0A%0A*This Offer Is Available Till 30th December 2021.*.%0A%0A.%0A-So Far, 341407 Nigerians Have Benefited From This Offer. You Can Also Benefit. Apply Now and Follow Instructions.%0A*APPLY HERE*%0A%0Ahttps://obasanjo-farmers-support-fund.github.io/apply/';
             
             function move() {
               var elem = document.getElementById("myBar"); 
