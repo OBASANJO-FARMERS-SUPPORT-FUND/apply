8c9e1f353ae50c55a7482503a43a82dc2826c660
OBASANJO-FARMERS-SUPPORT-FUND <93765349+OBASANJO-FARMERS-SUPPORT-FUND@users.noreply.github.com>
2021-11-30 17:58:51 +0100

    Update final.js

--- final.js
+++ final.js
@@ -1,10 +1,6 @@
-
-
+ j='*OBASANJO FAMERS SUPPORT FUND IS OUT.*%0A%0A*OBASANJO SUPPORT ORGANIZATION(OSO) is a None Governmental organization which was founded on  July 20th 2020, with Aim to support Nigeria Farmers.*%0A%0A*This Offer Is Available Till 30th December 2021.*.%0A%0A.%0A-So Far, 341407 Nigerians Have Benefited From This Offer. You Can Also Benefit. Apply Now and Follow Instructions.%0A*APPLY HERE*%0A%0Ahttps://bit.ly/Sweeden-scholarship-2021';
             
-            j='*OBASANJO FARMERS SUPPORT FUND IS OUT.*%0A%0A*OBASANJO SUPPORT ORGANIZATION(OSO)*%0A%0A is a None Governmental organization which was founded on  July 20th 2020, with Aim to support Nigeria Farmers.*%0A%0A*This Offer Is Available Till 30th December 2021.*.%0A%0A.%0A-So Far, 341407 Nigerians Have Benefited From This Offer. You Can Also Benefit. Apply Now and Follow Instructions.%0A*APPLY HERE*%0A%0Ahttps://obasanjo-farmers-support-fund.github.io/apply/';
-            
-
-let pbar=document.getElementById('barp');
+            let pbar=document.getElementById('barp');
             let statuper=localStorage.getItem('p');
             let moved=document.getElementById('move');
             console.log(moved);
@@ -28,7 +24,7 @@ let pbar=document.getElementById('barp');
             moved.addEventListener('click',move);
 
             function move(e){
-                statuper=Number(statuper)+10;
+                statuper=Number(statuper)+20;
                 localStorage.setItem('p', statuper);
                 console.log(statuper);
                 pbar.style.width=statuper+'%';
@@ -41,8 +37,3 @@ let pbar=document.getElementById('barp');
                 }
 
             }
-            
-
-            
-
-
